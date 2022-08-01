329f75c1bdfcedf54405cebdc68fc5c15f6759a6
GustavoRSL <d7gustavo@yahoo.com.br>
2022-08-01 14:24:20 -0300

    Esqueleto pronto, falta apenas realizar mutação e cross-over

--- algoritmo.js
+++ algoritmo.js
@@ -1,6 +1,7 @@
 var genes = [];
 var maxCromossomos = 30;
 var maxGeracoes = 20;
+var geracaoAtual = 0;
 
 // Intervalo do valores de X
 var min = -10;
@@ -32,25 +33,69 @@ function avaliacao(populacao) {
   for (individuo in populacao) {
     //Calculo de Fitness
     x = populacao[individuo].numero;
-    populacao[individuo].pontuacao = x*x-3*x+4;
+    populacao[individuo].pontuacao = x * x - 3 * x + 4;
   }
 }
 
 // Terceiro passo -> Enquanto critério de parada não for satisfeito
-function algoritmoGenetico(populacao){
-  for(var i=0; i<20; i++){
-    // Selecionar os indivíduos mais aptos.
+function algoritmoGenetico(populacao) {
+  var novaPopulacao = [];
+  // Selecionar os indivíduos mais aptos.
+  populacao.sort(function (a, b) { // Realiza uma ordenação por aqueles que possuem uma melhor pontuação
+    if (a.pontuacao > b.pontuacao) {
+      return -1;
+    }
+    if (a.pontuacao < b.pontuacao) {
+      return 1;
+    }
+    return 0;
+  })
+  pai = populacao[0];
+  mae = populacao[1];
+  achou = true;
+  // Garantir que os 2 resultados sejam diferentes.
+  for(individuo in populacao){
+    if(populacao[individuo].pontuacao != pai.pontuacao && achou == true){
+      mae = populacao[individuo];
+      achou = false;
+    }
+  }
+
+  if(geracaoAtual < maxGeracoes) { // Criterio de parada gerar 20 gerações
+
+    // Dois melhores resultados da população foram salvos.
+    novaPopulacao.push(pai);
+    novaPopulacao.push(mae);
 
     // Criar novos indivíduos aplicando os operadores crossover e mutação.
-
-    // Armazenar os novos indivíduos em uma nova população.
-
-    // Avaliar cada cromossomo da nova população.
+    for(var j=0; j<maxCromossomos-2; j++){
+      var novoCromossomo = new cromossomos();
+      // Mutação
+      var randomMutacao = Math.floor((Math.random()*100)+1); // Aplicar Mutação com taxa de 1%
+      if(randomMutacao == 1){
+        novoCromossomo.numero = randomInt(min, max)
+        novoCromossomo.binario = novoCromossomo.numero.toString(2);
+        console.log("Digimon digienvolve para ANTEDEGUEMON!!!"); 
+      }
+      else {
+        console.log("Pai:"+pai.binario)
+        console.log("Mãe: "+mae.binario)
+      }
+      // Armazenar os novos indivíduos em uma nova população.
+      novaPopulacao.push(novoCromossomo);
+    }
+    geracaoAtual++;
+    //Avaliar cada cromossomo da nova população.
+    avaliacao(novaPopulacao)
+    algoritmoGenetico(novaPopulacao);
+  }
+  else {
+    console.log("Acabou!")
+    //console.log(populacao)
   }
 }
 
 populacaoInicial();
 avaliacao(genes);
-console.log(genes)
 algoritmoGenetico(genes);
 
